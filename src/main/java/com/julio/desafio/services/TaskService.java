9cd9a3d3e3aedd9800a472eaaf20ec579adc6a44
julio <juliocesar13b@gmail.com>
2025-10-15 21:30:56 -0300

    criando service de criação de task

--- src/main/java/com/julio/desafio/services/TaskService.java
+++ src/main/java/com/julio/desafio/services/TaskService.java
@@ -1,7 +1,18 @@
 package com.julio.desafio.services;
 
+import com.julio.desafio.entity.Task;
+import com.julio.desafio.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    public Task createTask(Task newTask){
+       return taskRepository.save(newTask);
+    }
 }
