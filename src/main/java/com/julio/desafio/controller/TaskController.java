fc50464ebc7ee53b9a660c761cd71fd4bf802a65
julio <juliocesar13b@gmail.com>
2025-10-15 21:39:21 -0300

    ajustando TaskRequest com validation

---
+++ src/main/java/com/julio/desafio/controller/TaskController.java
@@ -0,0 +1,4 @@
+package com.julio.desafio.controller;
+
+public class TaskController {
+}
--- src/main/java/com/julio/desafio/dtos/TaskRequest.java
+++ src/main/java/com/julio/desafio/dtos/TaskRequest.java
@@ -3,13 +3,14 @@ package com.julio.desafio.dtos;
 import com.julio.desafio.entity.Project;
 import com.julio.desafio.enums.Priority;
 import com.julio.desafio.enums.Status;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
 
-public record TaskRequest(String title,
+public record TaskRequest(@NotBlank(message = "Título é obrigatório") String title,
                           String description,
-                          Status status,
-                          Priority priority,
-                          Date dueDate,
-                          Project projectId) {
+                         @NotBlank(message = "Status é obrigatório") Status status,
+                          @NotBlank(message = "Prioridade é obrigatória") Priority priority,
+                          @NotBlank(message = "Data Limite é obrigatória") Date dueDate,
+                          @NotBlank(message = "Projeto é obrigatório")Project projectId) {
 }
