6148b3387d1175c0d66312d100b0bda79dd1fb41
julio <juliocesar13b@gmail.com>
2025-10-15 21:34:59 -0300

    criando DTOs de Task

---
+++ src/main/java/com/julio/desafio/dtos/TaskResponse.java
@@ -0,0 +1,15 @@
+package com.julio.desafio.dtos;
+
+import com.julio.desafio.entity.Project;
+import com.julio.desafio.enums.Priority;
+import com.julio.desafio.enums.Status;
+
+import java.util.Date;
+
+public record TaskResponse(String title,
+                           String description,
+                           Status status,
+                           Priority priority,
+                           Date dueDate,
+                           Project projectId) {
+}
