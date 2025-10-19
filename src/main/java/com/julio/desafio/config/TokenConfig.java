fee0c2739c8d01d39430c64f5a06518ab6537847
julio <juliocesar13b@gmail.com>
2025-10-19 12:40:15 -0300

    criando metodo validate token no tokenconfig

--- src/main/java/com/julio/desafio/config/TokenConfig.java
+++ src/main/java/com/julio/desafio/config/TokenConfig.java
@@ -2,9 +2,12 @@ package com.julio.desafio.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.julio.desafio.entity.User;
 
 import java.time.Instant;
+import java.util.Optional;
 
 public class TokenConfig {
 
@@ -18,5 +21,22 @@ public class TokenConfig {
                 .withExpiresAt(Instant.now().plusSeconds(86400))
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> validadeToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build().verify(token);
+
+            return Optional.of(JWTUserData.bulder()
+                    .userId(decode.getClaim("userId").asLong())
+                    .email(decode.getSubject())
+                    .build());
+        }
+        catch (JWTVerificationException exception){
+            return Optional.empty();
+        }
     }
 }
