7ff558a9ef9fd14bde2c59eaa2100ee10b2132c8
julio <juliocesar13b@gmail.com>
2025-10-19 11:50:29 -0300

    criando repository de user

--- src/main/java/com/julio/desafio/repository/UserRepository.java
+++ src/main/java/com/julio/desafio/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.julio.desafio.repository;
 
-public interface UserRepository {
+import com.julio.desafio.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<UserDetails> findUserByEmail(String username);
 }
