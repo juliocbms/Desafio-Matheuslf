8a30fc7519f5372557a5f47e70789850405c008a
julio <juliocesar13b@gmail.com>
2025-11-12 21:53:40 -0300

    adicionando JSON serialization na classe RegisterUserRequest

--- src/main/java/com/julio/desafio/dtos/RegisterUserRequest.java
+++ src/main/java/com/julio/desafio/dtos/RegisterUserRequest.java
@@ -1,10 +1,13 @@
 package com.julio.desafio.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.julio.desafio.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterUserRequest(@NotBlank(message = "Nome é obrigatóri")String name,
+@JsonPropertyOrder({"nome_aqui","email","senha","role"})
+public record RegisterUserRequest(@NotBlank(message = "Nome é obrigatóri") @JsonProperty("nome_aqui") String name,
                                   @NotBlank(message = "Email é obrigatório") @Email String email,
                                   @NotBlank(message = "Senha é obrigatória")String senha,
                                   Role role) {
